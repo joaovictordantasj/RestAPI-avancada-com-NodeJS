c2dec8637cbebf93f7e0cfcccad97a04e238b37f
João Victor <jv@pop-os.localdomain>
2021-02-13 11:15:33 -0300

    Metodo GET criado

--- api/index.js
+++ api/index.js
@@ -4,4 +4,8 @@ const bodyParser = require('body-parser');
 const config = require('config');
 
 app.use(bodyParser.json());
+
+const roteador = require('./rotas/fornecedores')
+app.use('/api/fornecedores', roteador);
+
 app.listen(config.get('api.porta'), () => console.log('Api Funcionando'));
