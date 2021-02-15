8244d14c7cdfc79c8fe64d278f980334a941ce25
João Victor <joaovictordjardim@gmail.com>
2021-02-14 21:03:05 -0300

    Metodo GET aprimorado

--- api/rotas/fornecedores/index.js
+++ api/rotas/fornecedores/index.js
@@ -18,4 +18,16 @@ roteador.post('/', async (req, res) => {
   );
 });
 
+roteador.get('/:idFornecedor', async (req, res) => {
+  try {
+    const id = req.params.idFornecedor;
+    const fornecedor = new Fornecedor({ id: id });
+    await fornecedor.carregar();
+
+    res.send(JSON.stringify(fornecedor));
+  } catch (err) {
+    res.send(JSON.stringify({ Erro : err.message }));
+  }
+});
+
 module.exports = roteador;
