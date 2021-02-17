582d4f35c47327ea184660725e05484a7eac14d8
João Victor <joaovictordjardim@gmail.com>
2021-02-17 18:11:32 -0300

    Validacao de tipos de dados criada

--- api/index.js
+++ api/index.js
@@ -5,8 +5,28 @@ const config = require('config');
 const NaoEncontrado = require('./erros/NaoEncontrado');
 const CampoInvalido = require('./erros/CampoInvalido');
 const DadosNaoFornecidos = require('./erros/DadosNaoFornecidos');
+const ValorNaoSuportado = require('./erros/ValorNaoSuportado');
+const formatosAceitos = require('./Serializador').formatosAceitos;
 
 app.use(bodyParser.json());
+
+app.use((req, res, proximo) => {
+  let formatoRequisitado = req.header('Accept');
+
+  if (formatoRequisitado === '*/*') {
+    formatoRequisitado = 'application/json';
+  }
+
+  if (formatosAceitos.indexOf(formatoRequisitado) === -1) {
+    res.status(406);
+    res.end();
+    return;
+  }
+
+  res.setHeader('Content-Type', formatoRequisitado);
+  proximo();
+
+});
 
 const roteador = require('./rotas/fornecedores')
 app.use('/api/fornecedores', roteador);
@@ -21,6 +41,11 @@ app.use((err, req, res, proximo) => {
   if (err instanceof CampoInvalido || err instanceof DadosNaoFornecidos) {
     status = 400;
   }
+
+  if (err instanceof ValorNaoSuportado) {
+    status = 406;
+  }
+
 
   res.status(status);
 
